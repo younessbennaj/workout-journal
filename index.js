c5c5492eb20c1fa9ccd6c03b3ab44ee755aa55c4
Youness BENNAJ <youness.bennaj@gmail.com>
2020-07-07 17:49:41 +0200

    add jQuery cdn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log(Backbone);
+// var Users = Backbone.Collection.extend({
+//     url: 'https://jsonplaceholder.typicode.com/users'
+// });
+
+
+
+// var User = Backbone.Model.extend({}, )
+
+// var john = new User({ name: "John", age: 25 });
+
+// john.set('age', 29);
+
+// var object = {};
+
+// _.extend(object, Backbone.Events);
+
+// object.on('alert', function (data) {
+//     console.log('triggered: ' + data);
+// })
+
+// setTimeout(function () {
+//     object.trigger('alert', 'Hello World!');
+// }, 3000);
+
+//Make an AJAX request with jQuery to get our user and display it in the view
+
+
