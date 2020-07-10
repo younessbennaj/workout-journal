2c1f4f3b962faeac77f2efd8773779ac1e36fea2
Youness BENNAJ <youness.bennaj@gmail.com>
2020-07-10 17:35:12 +0200

    load users

--- index.js
+++ index.js
@@ -1,27 +1,93 @@
-// var Users = Backbone.Collection.extend({
-//     url: 'https://jsonplaceholder.typicode.com/users'
-// });
+/*/
+    * Backbone.Model *
+    
+        - Stores application data
+        - Defines default attributes
+        - Emits events when data attrites change
 
+    * Backbone.Collection *
 
+        - Manages a set of models
+        - Provides basic search methods on models
+        - Emits events when models are added, removed, sorted and/or changed.
+    
+    * Backbone.View *
+    
+        -  Connects a model to its visual representation in the the "DOM".
 
-// var User = Backbone.Model.extend({}, )
+    I) Backbone Component
 
-// var john = new User({ name: "John", age: 25 });
+    /!\ => When you extend a Backbone component => get back a constructor function.
+    
+/*/
 
-// john.set('age', 29);
+/*/
+    Backbone.Model.extend() => return a contructor function.
 
-// var object = {};
+        - url : a RESTful URL for our model to sync with
 
-// _.extend(object, Backbone.Events);
+/*/
 
-// object.on('alert', function (data) {
-//     console.log('triggered: ' + data);
-// })
+let apiUrl = "https://jsonplaceholder.typicode.com";
 
-// setTimeout(function () {
-//     object.trigger('alert', 'Hello World!');
-// }, 3000);
+//Create our model 
+//We store the returned constructor function in KermitModel variable
+var UserModel = Backbone.Model.extend({
+    defaults: {
+        id: null,
+        name: null,
+        occupation: null
+    }
+});
 
-//Make an AJAX request with jQuery to get our user and display it in the view
+//Create our collection
+//It will construct the loaded data from /users into a set of UserModel instances
+var UsersCollection = Backbone.Collection.extend({
+    url: `${apiUrl}/users`,
+    model: UserModel,
+    /*/
+    => needed if our root data structure is an Object (not an array like data.users).
+
+    parse: function(data) {
+        return data.users;
+      }
+    /*/
+});
+
+//We create a new instance of the collection thanks to the return constructor function
+let users = new UsersCollection();
+
+//fetch() => load data from its REST endpoint
+//return a promise object
+users.fetch().then(value => {
+    //When the promise is fullfiled with the loaded data from /user
+    //Then, users => a set of UserModel instances.
+    console.log(users.length);
+    console.log(users.get(1));
+
+})
+
+/*/
+
+    * Event and 'this' binding *
+
+    users.on('sync', () => {
+        console.log(this);
+    });
+
+    //is equivalent of
+
+    users.on('sync', function() {
+        console.log(this);
+    }, this);
+
+    => Context passing ('this' refer to the execution context)
+
+/*/
+
+users.on('sync', function () {
+    console.log(this);
+});
+
 
 
