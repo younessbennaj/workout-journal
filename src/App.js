43c663266b041eeb6a520e967e94ecf5392004fa
Youness BENNAJ <youness.bennaj@gmail.com>
2020-08-19 16:59:57 +0200

    init the sets form component

--- src/App.js
+++ src/App.js
@@ -1,71 +1,60 @@
 import React, { useState, useEffect } from 'https://cdn.skypack.dev/react@^16.13.1';
-// import { ListItem } from './ListItem.js';
-import { Table } from './Table.js';
 
-const props = {
-    foo: 'foo',
-    bar: 'bar'
+const Input = ({ label, update }) => {
+
+    function handleChange(e) {
+        update(e.target.value)
+    }
+
+    return (
+        <React.Fragment>
+            <label>{label}</label>
+            <input onChange={handleChange} type="text" />
+        </React.Fragment>
+    )
 }
 
-/*/
-
-    Function component => a JS function that returns a React Element object.
-
-    /!\ => Since React 16.8, function compoenent are more than "stateless components". Thanks
-    to hooks we can now use local state in those components. 
-
-/*/
-
-const App = ({ users: data }) => {
-    // We declare a "users" state variable
-    const [users, setUsers] = useState([]); //We pass an empty array as initial state.
-
-    /*/
-
-        Function component and http request => Generate a side effect.
-
-        Side effect: modify the state of something outside the scope of the 
-        function. 
-
-        When we use class component we put the side effect code inside the componentDidMount()
-        and componentDidUpdate() lifecycle methods. 
-
-        But, due to we want that the side effect should exectuted at the end of the 
-        mounting phase and when the component update, we need to duplicate the code
-        inside the two methods. This could be repetitive and hard to maintain. 
-
-        Thanks to the rise of react hooks, we have another solution: React.useEffect() hook.
-
-        useEffect(effect) => execute some code each time something is displayed.
-
-        effect: a function that will be called when the DOM is updated. (act as an async callback).
-
-        (synchronous side effect => useLayoutEffect() hook.)
-
-        /!\ => react component still must act as pure function when it comes to props.
-    
-    /*/
-
-    useEffect(() => {
-        //React guarantees that this code will be executed when the render is committed to the screen
-
-        //Also, thanks to closure, here we can acess to props values.
-
-        data.fetch().then(users => {
-            setUsers(users);
-        })
-
-        //if we return a function => React will use it to clean up the side effect
-        // => unmounting phase
-    }, []); //We we use an empty array as second argument, we tell to React that our effect depends neither of props of local state. So it don't need to be re executed.
-
-    //Our JSX code will be parsed by Babel, so the function will return a React Element. 
+const Form = ({ updateReps, updateWeight }) => {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h1>Table</h1>
-            <Table {...props} />
-        </div>
+        <form onSubmit={handleSubmit}>
+            <Input update={updateReps} label="repetition" />
+            <Input update={updateWeight} label="weight" />
+            <input type="submit" value="Add" />
+        </form>
     )
+}
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            reps: 0,
+            weight: 0
+        }
+        this.updateReps = this.updateReps.bind(this);
+        this.updateWeight = this.updateWeight.bind(this);
+    }
+
+    updateReps(reps) {
+        reps = Number.parseInt(reps);
+        this.setState({ reps });
+    }
+
+    updateWeight(weight) {
+        weight = Number.parseInt(weight);
+        this.setState({ weight });
+    }
+
+    render() {
+
+        return (
+            <Form updateReps={this.updateReps} updateWeight={this.updateWeight} />
+        );
+    }
 }
 
 export { App };
--- src/index.js
+++ src/index.js
@@ -1,101 +1,11 @@
-/*/
-    * Backbone.Model *
-    
-        - Stores application data
-        - Defines default attributes
-        - Emits events when data attrites change
 
-    * Backbone.Collection *
-
-        - Manages a set of models
-        - Provides basic search methods on models
-        - Emits events when models are added, removed, sorted and/or changed.
-    
-    * Backbone.View *
-    
-        -  Connects a model to its visual representation in the the "DOM".
-
-    I) Backbone Component
-
-    /!\ => When you extend a Backbone component => get back a constructor function.
-    
-/*/
-
-/*/
-    Backbone.Model.extend() => return a contructor function.
-
-        - url : a RESTful URL for our model to sync with
-
-/*/
 
 import * as React from 'https://cdn.skypack.dev/react@^16.13.1';
 import * as ReactDOM from 'https://cdn.skypack.dev/react-dom@^16.13.1';
 import { App } from './App.js';
 
-let apiUrl = "https://jsonplaceholder.typicode.com";
-
-//Create our model 
-//We store the returned constructor function in UserModel variable
-var UserModel = Backbone.Model.extend({
-    defaults: {
-        id: null,
-        name: null,
-        occupation: null
-    }
-});
-
-//Create our collection
-//It will construct the loaded data from /users into a set of UserModel instances
-var UsersCollection = Backbone.Collection.extend({
-    url: `${apiUrl}/users`,
-    model: UserModel,
-    /*/
-    => needed if our root data structure is an Object (not an array like data.users).
-
-    parse: function(data) {
-        return data.users;
-      }
-    /*/
-});
-
-//We create a new instance of the collection thanks to the return constructor function
-let users = new UsersCollection();
-
-//fetch() => load data from its REST endpoint
-//return a promise object
-users.fetch().then(value => {
-    //When the promise is fullfiled with the loaded data from /user
-    //Then, users => a set of UserModel instances.
-
-})
-
-/*/
-
-    * Event and 'this' binding *
-
-    users.on('sync', () => {
-        console.log(this);
-    });
-
-    //is equivalent of
-
-    users.on('sync', function() {
-        console.log(this);
-    }, this);
-
-    => Context passing ('this' refer to the execution context)
-
-/*/
-
-users.on('sync', function () {
-
-});
-
-/* VIEW */
-
-//React JS
 ReactDOM.render(
-    <App users={users} />,
+    <App />,
     document.getElementById('root')
 );
 
