77d8ac965add196cc2dedeeefc9812671d99f13c
Youness BENNAJ <youness.bennaj@gmail.com>
2020-08-28 16:12:54 +0200

    use preset-react preset from babel to preprocess JS file and convert JSX to vanilla JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 
 module.exports = {
+    mode: "development",
     //Entry point where webpack start to build dependencies graph 
-    entry: 'src/index.js',
+    entry: './src/index.js',
     //Where webpack put the compiled code 
     output: {
         //We need the absolute path
@@ -16,11 +17,11 @@ module.exports = {
             {
                 //We need to preprocess our JS files because of JSX syntax 
                 //We need to convert JSX to vanilla JS using Babel 
-                //Thanks to babel-loader package and babel react-preset we can do that
+                //Thanks to babel-loader package and babel preset-react we can do that
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-react']
                     }
                 },
                 //apply this loader if there is any file that matches this pattern (regular expression)
